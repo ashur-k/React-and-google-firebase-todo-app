f68d395f92c743567c00d30fd8ae04744d487ec1
ashur-k <ashurkanwal@yahoo.com>
2021-07-25 22:14:26 +0100

    added useEffect and finish hosting project using google firebase hoting services

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from './components/todo/Todo';
 // material UI imports
 import { FormControl, InputLabel, Input, Button } from '@material-ui/core';
 import './App.css';
+import db from './firebase';
+import firebase from 'firebase'
 
 function App() {
 
-  const [todos, setTodos] = useState([
-    'Finish React toutorial!!!',
-    'Finish Django toutorial!!!',
-    'Finish firebase toutorial!!!',
-  ]);
-
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  // when the app loads, we need to listen to 
+  // the database and fetch ne todos as they get added / removed
+  useEffect(() => {
+    // this code here.. fires when the app.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({
+        id: doc.id, 
+        todo: doc.data().todo
+      })))
+    })
+  }, []);
+
+
   const addTodo = (event) => {
     // this will fire when we click the button
     event.preventDefault()
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     setTodos([...todos, input])
     setInput('')
   }
