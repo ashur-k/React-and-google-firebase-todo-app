ea179e03684a8498ee186edd1a4cb178b817bfd5
ashur-k <ashurkanwal@yahoo.com>
2021-07-25 20:01:59 +0100

    installed material ui added material ui button in app.js, added preventdefault in addtodo funciton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    'Finish React toutorial!!!',
+    'Finish Django toutorial!!!',
+    'Finish firebase toutorial!!!',
+  ]);
+
+  const [input, setInput] = useState('');
+  const addTodo = (event) => {
+    // this will fire when we click the button
+    event.preventDefault()
+    setTodos([...todos, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-    
+
       <h1>Hello World</h1>
-      <input />
-      <button>Add Todo</button>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <Button type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </form>
+      
 
       <ul>
-        <li>Finish React toutorial</li>
-        <li>Finish Django toutorial</li>
-        <li>Finish firebase toutorial</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}        
       </ul>
 
     </div>
